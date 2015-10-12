75a97258cdf9bee828f90217cb3604049ead08b2
Andre Garcia <andrecadgarcia@gmail.com>
2015-10-12 16:33:05 -0300

    Added solution for exercise 1

---
+++ exercise1.asm
@@ -0,0 +1,14 @@
+org 0000h
+ljmp main
+
+;The goal is to increment a register to 100 and then decrement it until 0, redo the cycle infinitely
+org 0030h			
+main:
+	mov r1, #0    				;Set 0 to register 1
+increment:
+	inc r1						;Increment register 1 by 1
+	cjne r1, #100, incremento	;If register 1 is not equal to 100 small jump to increment lable
+decrement:	
+	dec r1						;Decrement register 1 by 1
+	cjne r1, #0, decremento		;If register 1 is not equal to 0 small jump to decrement lable
+	sjmp incremento 			;Small jump to increment label to redo the cycle
