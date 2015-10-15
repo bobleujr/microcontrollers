579cc4a494c8d57ebbeebe301cac59a45dfcb8c6
Andre Garcia <andrecadgarcia@gmail.com>
2015-10-15 02:05:41 -0300

    Added 750 ms case

--- exercise7.asm
+++ exercise7.asm
@@ -1,4 +1,4 @@
-;Same logic as exercise 3, but now using time to delay 1 second insted
+;Same logic as exercise 3, but now using time to delay 1 second and 750 miliseconds instead
 ;of nested increments. Note that the timer overflows happens each 50 ms
 ;so we used a counter to increment 20 times until something really happens,
 ;at least for the human.
@@ -11,10 +11,19 @@ org 0030h
 ;Function to handle timer interruption 0
 timer0:
 	inc r1					;Increment register 1 by 1
-	cjne r1, #20, exit		;If register 1 is not equal to 20 small jump to exit lable
+	mov A, r1				;Move r1 to A
+	subb A, r2				;Subtract r2 to A and store in A (A = A - r2)
+	cjne A, 0, exit			;If A is not equal to 0 small jump to exit lable
 	mov r1, #0				;Reset register 1
 	mov A, p1				;Move port 1 to A
-	rl A					;Rotate A to the left
+	cjne r2, #20, right		;If r2 is not equal to 20, small jump to right
+	rl A					;Rotate A to the right
+	mov r2, #15				;Set r2 to 15 (750 ms)
+	sjmp moveback
+right:
+	rr A					;Rotate A to the right
+	mov r2, #20				;Set r2 to 15 (1 s)
+moveback:
 	mov p1, A				;Move back A to port 1
 exit:
 	clr tf0					;Clear interruption timer flag
@@ -30,6 +39,7 @@ main:
 	mov th0, #00111100b		
 	mov tl0, #10101111b		;High and Low values make the number 500000 (which is used to count 50 ms)
 	mov r1, #0				;Set 0 to register 1
+	mov r2, #20
 	setb tr0				;Start timer 0
 	clr p1.0				;Turn on led in p1.0
 	sjmp $					;idle infinitely
