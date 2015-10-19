1a66236f20c32fe73b06fccb7bb2d064aa335b69
Andre Garcia <andrecadgarcia@gmail.com>
2015-10-18 23:23:07 -0200

    Added authors and fixed logic yet not working properly

--- exercise10.asm
+++ exercise10.asm
@@ -1,30 +1,42 @@
-;The sequence of leds to be turned on will be from 0 to 3 (4 leds from left to right),
-;the next led of this sequence will be in 0 and restart the cycle. Using timer the delay
-;for each switch is 100 ms, but if a character 'V' is received by serial, then the delay 
-;must be increased by 100 ms. When the sum reach 500 ms the next delay to be used must 
-;be 100 ms and restart the cycle.
+;
+ ;Authors: Andre, Cesar, Daniel, Paulo
+ ;The sequence of leds to be turned on will be from 0 to 3 (4 leds from left to right),
+ ;the next led of this sequence will be in 0 and restart the cycle. Using timer the delay
+ ;for each switch is 100 ms, but if a character 'V' is received by serial, then the delay 
+ ;must be increased by 100 ms. When the sum reach 500 ms the next delay to be used must 
+ ;be 100 ms and restart the cycle. If a character 'D' is received by serial, then the
+ ;direction must change.
+;
 org 0000h
 ljmp main
-org 0023h ;serial
+org 23h ;serial
 call serial						;call function handler
+reti                        	;return and reset flags
+org 000bh ;timer0
+call timer0						;call function handler
 reti               				;return and reset flags
 org 0030h
-;Function to handle serial interruption
+
 serial:
-	jnb ri, transmition			;If ri (reception) is equal to 0, small jump to transmition
-reception:
+	jnb ri, $					;While serial port is reading, keep it in same address
 	clr ri						;Clear ri
-	mov A, sbuf					;Move data from buffer to A
-	cjne A, #'D', velocity		;If not equal to 'D', call velocity
-	call direction				;Otherwise call direction
+	mov A, sbuf					;Move sbuf to A
+	cjne A, #'D', velocity		;If A is not 'D', small jump to velocity
+direction:
+	cjne r3, #0, subtract		;If r3 is not zero, small jump to subtract
+	mov r3, #1					;Set r3 to 1
+	clr ie1						;Clear extern interruption 1 flag
 	ret							;return
-transmition:
-	clr ti						;Clear ti
+subtract:
+	mov r3, #0					;Set r3 to 0
+	clr ie1						;Clear extern interruption 1 flag
 	ret							;return
+	
+;Function to handle character 'V'
 velocity:
 	mov A, #2					;Set A to 2
 	add A, r2					;Add A to r2 and store result in A (A = A+r2)
-	cjne r2, #10, sum			;If register 2 is not equal to 10 small jump to sum
+	cjne A, #12, sum			;If A is not equal to 12 small jump to sum
 	mov r2, #2					;Reset register 2
 	clr ie0						;Clear extern interruption 0 flag
 	ret             	        ;return
@@ -32,36 +44,54 @@ sum:
 	mov r2, A					;Move back A to r2 
 	clr ie0						;Clear extern interruption 0 flag
 	ret 	                    ;return
-direction:
+;Function to handle timer interruption 0
+timer0:
 	inc r1						;Increment register 1 by 1
 	mov A, r2					;Move r2 to A
 	subb A, r1					;Subtract r1 to A and store result in A (A = A-r1) which basically is A = r2 - r1
 	cjne A, #0, exit			;If result of subtraciton is not equal to 0, in other words r2 == r1, small jump to exit
 	mov r1, #0					;Reset register 1
 	mov A, p1					;Move port 1 to A
-	cjne A, #11110111b, rotate	;If led 4 is not turned on, small jump to rotate
-	setb p1.3					;Turn off led in p1.4
-	clr p1.7					;Turn on led in p1.7 (last position because the next operation is rotation to right so the turned on led will be in p1.0)
+	cjne r3, #1, left
+	cjne A, #11111110b, rotate	;If led 4 is not turned on, small jump to rotate
+	setb p1.0					;Turn off led in p1.4
+	clr p1.3					;Turn on led in p1.3
+	sjmp exit					;Small jump to exit
+left:
+	cjne A, #11110111b, rotate	;If led 0 is not turned on, small jump to rotate
+	setb p1.3					;Turn off led in p1.3
+	clr p1.0					;Turn on led in p1.0
+	sjmp exit                   ;Small jump to exit
 rotate:
+	cjne r3, #0, right			;If r3 is not 0, rotate port to right, otherwise to left
 	mov A, p1					;Move port 1 to A
 	rl A						;Rotate A to left
 	mov p1, A					;Move back A to p1
+	sjmp exit					;Small jump to exit
+right:
+	mov A, p1					;Move port 1 to A
+	rr A						;Rotate A to right
+	mov p1, A					;Move back A to p1
 exit:
+	clr tf0						;Clear interruption timer flag
+	clr tr0                     ;Stop timer (needs to be reseted)
+	mov th0, #00111100b         
+	mov tl0, #10101111b         ;High and Low values make the number 500000 (which is used to count 50 ms)
+	setb tr0                    ;Restart timer 0	
 	ret                         ;Return
 main:
-	;Set most significant bit (enable all interruptions), least significant bit (enable extern interrupt 0)
-	;and fourth most significant bit (enable serial)
-	mov ie, #10010000b
-;Set pair 01 in the most significant bits to use type 1 (8-bits UART)
-	;Set Fourth most significant bit (enable receive)
-	mov scon, #01010000b
-	;Set pair 10 in the third most significant bit (timer 1 as counter)
-	mov tmod, #00100000b
-	mov th1, #11111101b		
-	mov tl1, #11111101b			;High and Low values make the number 650021 (which makes baude rate 9600)
-	setb tr1					;Start timer 1
+	;Set most significant bit (enable all interruptions), fourth most significant bit (serial interrupt)
+	;and second least significant bit (enable timer interrupt 0)
+	mov ie, #10010100b
+	mov tmod, #21h				;Set timer 1 as counter and set timer 0 in type 1 (16-bit)
+	mov scon, #50h				;Set serial type 1 and enable receive
+	mov th0, #00111100b			
+	mov tl0, #10101111b         ;High and Low values make the number 500000 (which is used to count 50 ms)
+	mov th1, #-6				;Set baud rate to 4800
 	mov r1, #0                  ;Set 0 to register 1
 	mov r2, #2                  ;Set 2 to register 2
+	mov r3, #0					;If this register is 0, rotate left, if not, rotate right
 	setb tr0                    ;Start timer 0                
+	setb tr1                    ;Start timer 0                
 	clr p1.0    				;Turn on led in p1.0
 	sjmp $                      ;idle infinitely
--- exercise9.asm
+++ exercise9.asm
@@ -1,5 +1,8 @@
-;When a button placed in p3.2 is pressed the port 1 is transmitted via serial.
-;Any value send to 8051 by serial is put in port 1
+;
+ ;Authors: Andre, Cesar, Daniel, Paulo
+ ;When a button placed in p3.2 is pressed the port 1 is transmitted via serial.
+ ;Any value send to 8051 by serial is put in port 1
+;
 org 000h
 ljmp main
 org 03h ;int0
@@ -11,31 +14,107 @@ reti                        ;return and reset flags
 org 0030h
 ;Function to handle timer interruption 0
 int0:
-	setb ti					;Set serial to transmition mode
+	call printPort			;Call function to send "Port 1.'r1' - "
+	inc r1					;Increment port address
+	call printValue			;Call function to send "On" or "Off"
+	mov A, #'\n'			;send new line to be transmitted
+	call transmition
+	cjne r1, #34h, int0		;If register 1 is not equal to 34, loop int0
 	clr ie0					;Clear extern interruption 0 flag
-	reti                    ;return
+	mov r1, #30h			;reset register 1 to 30h
+	ret                     ;return
 ;Function to handle serial trasmition
 serial:
-jnb ri, transmition			;If ri (reception) is equal to 0, small jump to transmition
-reception:
+	jnb ri, $				;While serial port is reading, keep it in same address
 	clr ri					;Clear ri
-	mov p1, sbuf			;Move data from buffer to port 1
+	mov A, sbuf				;Move which has been sent to A	
+   	cjne A, #30h, p11		;If A is not 30h, small jump to p11
+	jnb p1.0, c10			;If p1.0's value is not 0, small jump to c10
+	setb p1.0				;Otherwise, turn off led in p1.0
+	sjmp exit				;Small jump to exit
+c10:
+	clr p1.0				;Turn on led in p1.0
+	sjmp exit				;Small jump to exit
+p11:
+   	cjne A, #31h, p12		;If A is not 31h, small jump to p12
+	jnb p1.1, c11           ;If p1.1's value is not 0, small jump to c11
+	setb p1.1               ;Otherwise, turn off led in p1.0
+	sjmp exit               ;Small jump to exit
+c11:                        
+	clr p1.1                ;Turn on led in p1.1
+	sjmp exit               ;Small jump to exit
+p12:
+   	cjne A, #32h, p13		;If A is not 32h, small jump to p13
+	jnb p1.2, c12           ;If p1.2's value is not 0, small jump to c12
+	setb p1.2               ;Otherwise, turn off led in p1.0
+	sjmp exit               ;Small jump to exit
+c12:                        
+	clr p1.2                ;Turn on led in p1.2
+	sjmp exit               ;Small jump to exit
+p13:
+	jnb p1.3, c13			;If p1.3's value is not 0, small jump to c13
+	clr p1.3                ;Otherwise, turn off led in p1.0
+	sjmp exit               ;Small jump to exit
+c13:                        
+	setb p1.3               ;Turn on led in p1.3
+exit:                       
 	ret						;return
+;Function to transmit a data
 transmition:
+	mov sbuf, A				;Move A to sbuf
+	jnb ti, $				;While sbuf was not entirely transmitted, keep it in same address
 	clr ti					;Clear ti
-	mov sbuf, p1			;Move port 1 to buffer
 	ret						;return
+;Function to send 'Port 1.r1 - ' by serial
+printPort:
+	mov A, #'P'
+	call transmition
+	mov A, #'o'
+	call transmition
+	mov A, #'r'
+	call transmition
+	mov A, #'t'
+	call transmition
+	mov A, #' '
+	call transmition
+	mov A, #'1'
+	call transmition
+	mov A, #'.'
+	call transmition
+	mov A, r1
+	call transmition
+	mov A, #'-'
+	call transmition
+	mov A, #' '
+	call transmition
+	ret
+;Function to send 'On' or 'Off' depending on port 1.r1's value 
+printValue:
+	mov A,r1				;Move r1 to A
+	add A, 60h				;Add A to address 60h
+	cjne A, #0b, off		;if A's value is not 0, small jump to off
+	mov A, #'O'				;Send "On" to be sent by serial
+	call transmition
+	mov A, #'n'
+	call transmition
+	ret						;return
+off:
+	mov A, #'O'				;Send "Off" to be sent by serial
+	call transmition
+	mov A, #'f'
+	call transmition
+	mov A, #'f'
+	call transmition
+	ret						;return 
 main:
 	;Set most significant bit (enable all interruptions), least significant bit (enable extern interrupt 0)
 	;and fourth most significant bit (enable serial)
-	mov ie, #10010001b
-	;Set pair 01 in the most significant bits to use type 1 (8-bits UART)
-	;Set Fourth most significant bit (enable receive)
-	mov scon, #01010000b
-	;Set pair 10 in the third most significant bit (timer 1 as counter)
-	mov tmod, #00100000b
-	mov th1, #11111101b		
-	mov tl1, #11111101b		;High and Low values make the number 650021 (which makes baude rate 9600)
+	mov ie, #10010001b		
+	mov tmod, #20h			;Set timer 1 as counter
+	mov th1, #-6			;Set baud rate to 4800
+	mov scon, #50h			;Set serial type 1 and enable receive
+	mov r1, #30h			;Set register 1 to 30h 
 	setb tr1				;Start timer 1
 	setb p3.2               ;Set interrupt button 0
+	setb it0				;Turn interruption 0 by falling edge
 	sjmp $                  ;idle infinitely
